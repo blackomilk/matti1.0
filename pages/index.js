559ba7e3358e7d9f970b0321340dd01a5379d188
blackomilk <siyonghang@365rili.com>
2020-07-17 21:00:57 +0800

    add music

--- pages/index.js
+++ pages/index.js
@@ -1,205 +1,122 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
+let isMusicUp = 0;
+const handleMusicUp = ()  =>{
+  const audio = document.getElementById('bgc');
+  // const audio = document.getElementsByTagName('source')
+  if(audio != null) {
+    if(audio.paused) {
+      audio.play();
+      isMusicUp = isMusicUp+1;
+    } else {
+      audio.pause();
+      console.log("sasasasa")
+    }
+  }
+}
+
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
+      <title>Matti</title>
+      <link rel="icon" href="/matti.png" />
+      <audio autoPlay loop id="bgc">
+        <source src="/bgm.mp3" type="audio/mpeg"></source>
+      </audio>
     </Head>
 
     <main>
-      <h1 className="title">
-        {/* Welcome to <a href="https://nextjs.org">Next.js!</a> */}
-        Read <Link href="/post/first-post"><a>this page!</a></Link>
-
-      </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </p>
-        </a>
-      </div>
+        {/* <h1>
+          <Link href="/post/first-post"><a>old</a></Link> */}
+        {/* </h1> */}
+        <header>
+            <div className="info">
+              <img src="/matti.png" className="icon"></img>
+            </div>
+            <div className="name">
+              <span>Matti</span>
+            </div>
+            <div className="music" onClick={() => handleMusicUp()}>
+              <img src={isMusicUp==0? "/bg.jpg" : ""} className="music-img"></img>
+            </div>
+        </header>
+        <div>
+          <div>
+            <div className="first-main">
+              <img src=""></img>
+              <h1>Hello I.am Matti</h1>
+            </div>
+          </div>
+        </div>
     </main>
 
     <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/vercel.svg" alt="Vercel Logo" />
-      </a>
+      
     </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
+    <style jsx>
+      {`
+        h1 {
+          font-size: 70px;
+          color: #FFFFFF;
+          background-color: #000;
         }
-      }
-    `}</style>
+        header {
+          width: 100%;
+          height: 100px;
+          padding-top: 20px
+        }
+        header div {
+          display: inline-block;
+        }
+        .info {
+          float: left;
+          margin-left: 5%;
+          height: 100%;
+        }
+        header img {
+          height: 50px;
+          border: 1px solid #FFF;
+          border-radius: 50%;
+          box-shadow: 0 0 15px #FFF;
+          margin: 0px 10px 0px 0px;
+        }
+        .name {
+          position: absolute;
+          background-color: #FFF;
+          font-size: 20px;
+          font-width: 500;
+          padding: 5px 15px;
+          border-radius: 10px;
+          margin-top: 10px;
+        }
+        .music {
+          float: right;
+          margin-right: 5%;
+          cursor: pointer;
 
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
+        }
+        .first-main {
+          background-color: #f0f0f0;
+          width: 800px;
+          margin: 10vh auto;
+        }
+      `}
+    </style>
+    <style jsx global>
+      {`
+      html,body {
         margin: 0;
+        padding: 0;
+        background-color: #000000;
         font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
           Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
       }
-
       * {
         box-sizing: border-box;
       }
-    `}</style>
+      `}
+    </style>
   </div>
 )
 
