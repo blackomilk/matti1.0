9a00ceeeddd8f998b45cc1105a8be7abd2d896dc
blackomilk <siyonghang@365rili.com>
2020-07-25 17:47:43 +0800

    upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/global.css'
+import '../swiper/swiper.min.css'
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,38 @@
 import Head from 'next/head'
-import Link from 'next/link'
+import React, { Component,useState } from 'react'
 
-let isMusicUp = true;
 
-const handleMusicUp = ()  =>{
-  const audio = document.getElementById('bgc');
-  // const audio = document.getElementsByTagName('source')
-  if(audio != null) {
-    if(audio.paused) {
-      audio.play();
-      isMusicUp = false;
-      console.log("sasasasa",isMusicUp)
-    } else {
-      audio.pause();
-      console.log("sasasasa")
+const Home = () => {
+  const [musicImg,setmusicImg] = useState('/timg.jpeg');
+
+  // a()
+  const handleMusicUp = ()  =>{
+    const audio = document.getElementById('bgc');
+    const img = document.getElementById('bgcm');
+    console.log("初始状态",img.style.animationPlayState)
+    if(img.style.animationPlayState = "running") {
+      img.style.animationPlayState = "paused"
+      img.style.webkitAnimationPlayState = "paused"
+      // console.log("暂停状态",img.style.animationPlayState)
+    } 
+    if(audio != null) {
+      if(audio.paused) {
+        setmusicImg('/timg.jpeg')
+        audio.play();
+        img.style.animationPlayState = "running"
+      } else {
+        setmusicImg('/bg.jpg')
+        audio.pause();
+      }
     }
   }
-}
 
-const Home = () => (
-  <div className="container">
+  return (
+    <>
     <Head>
       <title>Matti</title>
       <link rel="icon" href="/matti.png" />
     </Head>
-
     <main>
         <header>
             <div className="info">
@@ -33,19 +41,16 @@ const Home = () => (
             <div className="name">
               <span>Matti</span>
             </div>
-            <div className="music" onClick={() => handleMusicUp()}>
+            <div className="music" id="bgcm" onClick={() => handleMusicUp()}>
             <audio autoPlay  loop id="bgc">
               <source src="/bgm.mp3" type="audio/mpeg"></source>
             </audio>
-            {isMusicUp === false ? 
-             <img src="/bg.jpg" className="music-img"></img> : 
-             <img src="/timg.jpeg" className="music-img"></img>
-            }
+             <img src={musicImg} className="music-img"></img> 
           </div>
         </header>
-        <div className="body-main">
-          <div className="body-main-page">
-            <div className="body-main-page-one">
+        <div className='swiper-container' className="body-main" id="swiper-pc" >
+          <div className='swiper-wrapper' className="body-main-page" >
+            <div className="swiper-slide" className="body-main-page-one" >
               <div className="body-main-page-one-bottom">
                 <div className="body-main-page-one-bottomDown">
                 <div className="body-main-page-one-bottomUp">
@@ -61,7 +66,7 @@ const Home = () => (
                 </div>
               </div>
             </div>
-            <div className="body-main-page-two">
+            <div className="swiper-slide" className="body-main-page-two">
             <div className="body-main-page-two-bottom">
                 <div className="body-main-page-two-bottomDown">
                 <div className="body-main-page-two-bottomUp">
@@ -77,60 +82,28 @@ const Home = () => (
                 </div>
               </div>
             </div>
-            <div className="body-main-page-three">
-            <div className="body-main-page-one-bottom">
-                <div className="body-main-page-one-bottomDown">
-                <div className="body-main-page-one-bottomUp">
-                  <div className="body-main-page-one-bottomUp-logo">
-                    <img src="/mattti.png"></img>
-                  </div>
-                  <div className="body-main-page-one-bottomUp-title">
-                    <p>Hello I‘m Matti</p>
-                    <p>正在努力成为一名前端开发工程师</p>
-                    <p>@2020.07.18,星期六</p>
-                  </div>
-                </div>
-                </div>
-              </div>
+            <div className="swiper-slide" className="body-main-page-three">
+            
             </div>
-            <div className="body-main-page-four">
-            <div className="body-main-page-one-bottom">
-                <div className="body-main-page-one-bottomDown">
-                <div className="body-main-page-one-bottomUp">
-                  <div className="body-main-page-one-bottomUp-logo">
-                    <img src="/mattti.png"></img>
-                  </div>
-                  <div className="body-main-page-one-bottomUp-title">
-                    <p>Hello I‘m Matti</p>
-                    <p>正在努力成为一名前端开发工程师</p>
-                    <p>@2020</p>
-                  </div>
-                </div>
-                </div>
-              </div>
+            <div className="swiper-slide" className="body-main-page-four">
             </div>
-            <div className="body-main-page-five">
-            <div className="body-main-page-one-bottom">
-                <div className="body-main-page-one-bottomDown">
-                <div className="body-main-page-one-bottomUp">
-                  <div className="body-main-page-one-bottomUp-logo">
-                    <img src="/mattti.png"></img>
-                  </div>
-                  <div className="body-main-page-one-bottomUp-title">
-                    <p>Hello I‘m Matti</p>
-                    <p>正在努力成为一名前端开发工程师</p>
-                    <p>@2020.07.18,星期六</p>
-                  </div>
-                </div>
-                </div>
-              </div>
+            <div className="swiper-slide" className="body-main-page-five">
+            
             </div>
           </div>
-        </div>          
+        </div>
+        {/* <div className="swiper-pagination">
+            <span className="swiper-pagination-bullet">aaa</span>
+            <span className="swiper-pagination-bullet">aaa</span>
+            <span className="swiper-pagination-bullet">aaa</span>
+            <span className="swiper-pagination-bullet">aaa</span>
+            <span className="swiper-pagination-bullet">aaa</span>
+        </div>    */}
     </main>
-    <footer>
-    </footer>
-  </div>
-)
+    <footer></footer>     
+
+   </>
+  )
+}
 
 export default Home
